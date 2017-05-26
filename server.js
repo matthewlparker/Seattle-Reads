a8b392ca202186c3b132743c4c1c3e6e229d2239
Matthew Parker <cadburylion@outlook.com>
2017-05-26 16:55:11 -0700

    Added .get function to route traffic through server.js to index.html

--- server.js
+++ server.js
@@ -7,3 +7,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.static('./public'));
+
+app.get('*', (request, response) =>
+  response.sendFile('index.html', {root: './public'}));
+
+app.listen(PORT, function() {
+
+  console.log('My Portfolio is open on port: ' + PORT);
+});
