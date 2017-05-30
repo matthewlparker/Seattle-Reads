2a0d6728869f7464c31a6199bbdfaa3a6b12fb27
Matthew Parker <cadburylion@outlook.com>
2017-05-29 23:40:33 -0700

    Combined RegEx expressions into single line

--- public/scripts/view/app.js
+++ public/scripts/view/app.js
@@ -7,8 +7,6 @@ var books = [];
 
 (function(module) {
 
-
-
   function seattleReads(){
     $.ajax({
       url: 'https://data.seattle.gov/resource/tjb6-zsmc.json',
@@ -37,8 +35,8 @@ var books = [];
 
     books.forEach(function(book){
       console.log(book);
-      book.title = book.title.replace(/\/.*/, '');
-      book.title = book.title.replace(/\[.*?\]/, '');
+      book.title = book.title.replace(/\/.*/, '').replace(/\[.*?\]/, '');
+      // book.title = book.title
       let context = {title: `${book.title}`, type: `The medium is: ${book.usageclass}`};
       let html = template(context);
       console.log(html);
