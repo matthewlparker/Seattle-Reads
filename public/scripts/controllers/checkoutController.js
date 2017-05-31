9a0c918f0649415a334d41f345b54ee3adf339e5
Ryan Turner <ryanthepit@gmail.com>
2017-05-31 14:09:10 -0700

    refactored regexp again, to be dynamic

--- public/scripts/controllers/checkoutController.js
+++ public/scripts/controllers/checkoutController.js
@@ -26,72 +26,31 @@ var app = app || {};
   //   {name: 'sarc', subject: 'scar is a total loser'}
   //   ];
   //
-  checkoutController.fiction = [];
-  checkoutController.fantasy = [];
-  checkoutController.comedy = [];
-  checkoutController.romance = [];
-  checkoutController.sciFi = [];
-  checkoutController.music = [];
-  checkoutController.history = [];
+  let genres = ['Fiction', 'Fantasy', 'Comedy', 'Romantic', 'Sci\-Fi', 'Music', 'History'];
+  let genresRegExp = RegExp(genres.join('|'), 'g');
+  console.log(genresRegExp);
   checkoutController.other = [];
 
   function sortGenre(){
     books.forEach(function(checkout){
-      checkoutController.fiction = books.filter(function(checkout){
-        return findWord('Fiction').test(checkout.subjects);
-      });
-      checkoutController.fantasy = books.filter(function(checkout){
-        return findWord('Fantasy').test(checkout.subjects);
-      });
-      checkoutController.comedy= books.filter(function(checkout){
-        return findWord('Comedy').test(checkout.subjects);
-      });
-      checkoutController.romantic = books.filter(function(checkout){
-        return findWord('Romantic').test(checkout.subjects);
-      });
-      checkoutController.sciFi = books.filter(function(checkout){
-        return findWord('Sci-Fi').test(checkout.subjects);
-      });
-      checkoutController.music = books.filter(function(checkout){
-        return findWord('Music').test(checkout.subjects);
-      });
-      checkoutController.music = books.filter(function(checkout){
-        return findWord('Songs').test(checkout.subjects);
-      });
-      checkoutController.history = books.filter(function(checkout){
-        return findWord('History').test(checkout.subjects);
-      });
-      checkoutController.documentary = books.filter(function(checkout){
-        return findWord('Documentary').test(checkout.subjects);
-      });
-      checkoutController.other = books.filter(function(checkout){
-        return findWord(!'Other').test(checkout.subjects);
-      });
+      if(!checkout.subjects){
+        checkoutController.other.push(checkout)
+      } else {
+        let matches = checkout.subjects.match(genresRegExp);
+        if(matches){
+          matches.forEach(function(genre){
+            if(!checkoutController[genre]){
+              checkoutController[genre] = [];
+            }
+            checkoutController[genre].push(checkout);
+          });
+        } else {
+          checkoutController.other.push(checkout);
+        }
+      }
     });
-    console.log('Fiction', checkoutController.fiction);
-    console.log('Fantasy', checkoutController.fantasy);
-    console.log('Comedy', checkoutController.comedy);
-    console.log('Romantic', checkoutController.romantic);
-    console.log('Sci-Fi', checkoutController.sciFi);
-    console.log('Music', checkoutController.music);
-    console.log('Songs', checkoutController.music);
-    console.log('History', checkoutController.history);
-    console.log('Documentary', checkoutController.documentary);
-    console.log('Other', checkoutController.other);
-    // checkoutController.fiction = books.filter(function(checkout){
-    //   return findWord('Fiction').test(checkout.subjects);
-    // });
-    // console.log(books);
-    // console.log(checkoutController.fiction);
-    //
-    // checkoutController.fiction = books.filter(function(checkout){
-    //   return findWord('Romance').test(checkout.subjects);
-    // });
-    // console.log(checkoutController.romance);
-  }
-  function findWord(word) {
-      // console.log(RegExp(word, 'g'));
-    return RegExp(word);
+    console.log(checkoutController);
+
   }
 
   module.checkoutController = checkoutController;
