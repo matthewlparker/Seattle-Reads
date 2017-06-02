bf013a26f6245314f4bcebbb3d5a5b3a8f2ffbc7
Kyle Aardal <aardal.kyle@gmail.com>
2017-06-01 18:54:03 -0700

    fixed opacity styling and chart font

--- public/scripts/models/checkoutChart.js
+++ public/scripts/models/checkoutChart.js
@@ -12,6 +12,7 @@ var app = app || {};
     let bookCheckouts = books.map(book => book.checkouts);
 
     let ctx = document.getElementById(barCanvas).getContext('2d');
+    Chart.defaults.global.defaultFontColor = 'black';
     checkoutChart.myBarChart = new Chart(ctx, {
       type: 'bar',
       data: {
@@ -20,32 +21,32 @@ var app = app || {};
           label: '# of Checkouts',
           data: bookCheckouts,
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgba(255,99,132, .8)',
+            'rgba(54, 162, 235, .8)',
+            'rgba(255, 206, 86, .8)',
+            'rgba(75, 192, 192, .8)',
+            'rgba(153, 102, 255, .8)',
+            'rgba(255, 159, 64, .8)',
+            'rgba(255, 99, 132, .8)',
+            'rgba(54, 162, 235, .8)',
+            'rgba(255, 206, 86, .8)',
+            'rgba(75, 192, 192, .8)',
+            'rgba(153, 102, 255, .8)',
+            'rgba(255, 159, 64, .8)'
           ],
           borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgba(0, 0, 0, 1)',
+            'rgba(0, 0, 0, 1)',
+            'rgba(0, 0, 0, 1)',
+            'rgba(0, 0, 0, 1)',
+            'rgba(0, 0, 0, 1)',
+            'rgba(0, 0, 0, 1)',
+            'rgba(0, 0, 0, 1)',
+            'rgba(0, 0, 0, 1)',
+            'rgba(0, 0, 0, 1)',
+            'rgba(0, 0, 0, 1)',
+            'rgba(0, 0, 0, 1)',
+            'rgba(0, 0, 0, 1)',
           ],
           borderWidth: 1
         }]
@@ -79,14 +80,26 @@ var app = app || {};
           datasets: [{
             data: genreLengths,
             backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 99, 132, .8)',
+              'rgba(54, 162, 235, .8)',
+              'rgba(255, 206, 86, .8)',
+              'rgba(75, 192, 192, .8)',
+              'rgba(153, 102, 255, .8)',
+              'rgba(255, 159, 64, .8)',
+              'rgba(255, 99, 132, .8)',
+              'rgba(54, 162, 235, .8)',
+            ],
+            borderColor: [
+              'rgba(0, 0, 0, 1)',
+              'rgba(0, 0, 0, 1)',
+              'rgba(0, 0, 0, 1)',
+              'rgba(0, 0, 0, 1)',
+              'rgba(0, 0, 0, 1)',
+              'rgba(0, 0, 0, 1)',
+              'rgba(0, 0, 0, 1)',
+              'rgba(0, 0, 0, 1)',
+              'rgba(0, 0, 0, 1)',
+              'rgba(0, 0, 0, 1)',
             ]
           }]
         }
