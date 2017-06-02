629fc5333892dfe9eae629a63eee92e5fc6daa80
mathewcmartin <chactasstryker@gmail.com>
2017-06-01 21:07:28 -0700

    Committing changes to index.html and layout.css to style the about us link and relevant data.

--- public/scripts/views/aboutView.js
+++ public/scripts/views/aboutView.js
@@ -1,18 +1,18 @@
-'use strict';
-
-var app = app || {};
-
-(function(module){
-
-  const aboutView = {};
-
-  //this renders each element to the DOM
-  aboutView.renderResults = function () {
-    $('#about-div').append("<img src='./images/IMG951723.jpg' />");
-    $('#about-div').append("<img src='./images/Me.jpg' />");
-    $('#about-div').append("<img src='./images/ryanturner.jpg' />");
-    $('#about-div').append("<img src='./images/mathewcmartin.jpg' />");
-  };
-
-  module.aboutView = aboutView;
-})(app);
+// 'use strict';
+//
+// var app = app || {};
+//
+// (function(module){
+//
+//   const aboutView = {};
+//
+//   aboutView.renderResults = function () {
+//     $('#about-div').empty();
+//     $('#photoKA').append("<img src='./images/IMG951723.jpg' />");
+//     $('#photoMP').append("<img src='./images/Me.jpg' />");
+//     $('#photoRT').append("<img src='./images/ryanturner.jpg' />");
+//     $('#photoMM').append("<img src='./images/mathewcmartin.jpg' />");
+//   };
+//
+//   module.aboutView = aboutView;
+// })(app);
